056bb5d38412222c998b22de92cdd40130a73e06
Administrator <1726245362@qq.com>
2019-09-04 19:48:38 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapp/login.java
@@ -0,0 +1,16 @@
+package com.example.myapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class login extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.complex_activity_main);
+
+    }
+}
