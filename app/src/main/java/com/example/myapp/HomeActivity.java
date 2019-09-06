a3cc73566b59de5da30dac2aee2253d4857f00f3
Administrator <1726245362@qq.com>
2019-09-06 16:24:26 +0800

    更改界面连接地图功能可以先调试

--- app/src/main/java/com/example/myapp/HomeActivity.java
+++ app/src/main/java/com/example/myapp/HomeActivity.java
@@ -38,29 +38,29 @@ public class HomeActivity extends AppCompatActivity {
     }
 
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
-        mMapView.onDestroy();
-    }
-    @Override
-    public void onResume() {
-        super.onResume();
-        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
-        mMapView.onResume();
-    }
-    @Override
-    public void onPause() {
-        super.onPause();
-        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
-        mMapView.onPause();
-    }
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
-        mMapView.onSaveInstanceState(outState);
-    }
+//    @Override
+//    public void onDestroy() {
+//        super.onDestroy();
+//        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
+//        mMapView.onDestroy();
+//    }
+//    @Override
+//    public void onResume() {
+//        super.onResume();
+//        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
+//        mMapView.onResume();
+//    }
+//    @Override
+//    public void onPause() {
+//        super.onPause();
+//        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
+//        mMapView.onPause();
+//    }
+//    @Override
+//    public void onSaveInstanceState(Bundle outState) {
+//        super.onSaveInstanceState(outState);
+//        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
+//        mMapView.onSaveInstanceState(outState);
+//    }
 
 }
